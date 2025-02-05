1f05965f409204f80e99fa3c2a874389dda55213
Afifa <afifak14@gmail.com>
2025-02-05 05:08:14 -0500

    Remove files

--- q4.py
+++
@@ -1,9 +0,0 @@
-#. Temperature of a city in Fahrenheit degrees is input through the keyboard. Write a
-#program to convert this temperature into Centigrade degrees.
-
-
-Faren=float(input("Enter temp in Fahrenheit"))
-Celcius=round((Faren-32)*(5/9),2)
-
-print("Temp in Celcius=", Celcius,"C")
-
--- q6.py
+++
@@ -1,8 +0,0 @@
-#Two numbers (base and exponent) are entered through the keyboard. Write a program to
-#find the value of base raised to the power of exponent.
-
-base=float(input("enter base"))
-exp=float(input("Enter exponent"))
-
-value= base**exp
-print("Value is", value)
